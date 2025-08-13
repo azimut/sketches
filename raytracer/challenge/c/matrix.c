fa2a1fd22f5807294e27fbc1d070b03195d48398
azimut <azimut.github@protonmail.com>
2025-08-13 18:10:23 -0300

    raytracer: adding chapter 3 matrix mul/transpose, missing invert

--- raytracer/challenge/c/matrix.c
+++ raytracer/challenge/c/matrix.c
@@ -22,16 +22,45 @@ Mat4 m4_set(Mat4 m4, size_t row, size_t col, float val) {
   return m4;
 }
 
-bool m4_equal(Mat4 m, Mat4 n) {
-  return near(m.m[0][0], n.m[0][0]) && near(m.m[0][1], n.m[0][1]) &&
-         near(m.m[0][2], n.m[0][2]) && near(m.m[0][3], n.m[0][3]) &&
-         near(m.m[1][0], n.m[1][0]) && near(m.m[1][1], n.m[1][1]) &&
-         near(m.m[1][2], n.m[1][2]) && near(m.m[1][3], n.m[1][3]) &&
-         near(m.m[2][0], n.m[2][0]) && near(m.m[2][1], n.m[2][1]) &&
-         near(m.m[2][2], n.m[2][2]) && near(m.m[2][3], n.m[2][3]) &&
-         near(m.m[3][0], n.m[3][0]) && near(m.m[3][1], n.m[3][1]) &&
-         near(m.m[3][2], n.m[3][2]) && near(m.m[3][3], n.m[3][3]);
+bool m4_equal(Mat4 a, Mat4 b) {
+  return near(a.m[0][0], b.m[0][0]) && near(a.m[0][1], b.m[0][1]) &&
+         near(a.m[0][2], b.m[0][2]) && near(a.m[0][3], b.m[0][3]) &&
+         near(a.m[1][0], b.m[1][0]) && near(a.m[1][1], b.m[1][1]) &&
+         near(a.m[1][2], b.m[1][2]) && near(a.m[1][3], b.m[1][3]) &&
+         near(a.m[2][0], b.m[2][0]) && near(a.m[2][1], b.m[2][1]) &&
+         near(a.m[2][2], b.m[2][2]) && near(a.m[2][3], b.m[2][3]) &&
+         near(a.m[3][0], b.m[3][0]) && near(a.m[3][1], b.m[3][1]) &&
+         near(a.m[3][2], b.m[3][2]) && near(a.m[3][3], b.m[3][3]);
 }
+
+Mat4 m4_mul(Mat4 a, Mat4 b) {
+  Mat4 res;
+  for (size_t row = 0; row < 4; ++row) {
+    for (size_t col = 0; col < 4; ++col) {
+      res.m[row][col] = a.m[row][0] * b.m[0][col] + a.m[row][1] * b.m[1][col] +
+                        a.m[row][2] * b.m[2][col] + a.m[row][3] * b.m[3][col];
+    }
+  }
+  return res;
+}
+
+Tuple m4_tmul(Mat4 m, Tuple t) {
+  return (Tuple){
+      m.m[0][0] * t.x + m.m[0][1] * t.y + m.m[0][2] * t.z + m.m[0][3] * t.w,
+      m.m[1][0] * t.x + m.m[1][1] * t.y + m.m[1][2] * t.z + m.m[1][3] * t.w,
+      m.m[2][0] * t.x + m.m[2][1] * t.y + m.m[2][2] * t.z + m.m[2][3] * t.w,
+      m.m[3][0] * t.x + m.m[3][1] * t.y + m.m[3][2] * t.z + m.m[3][3] * t.w,
+  };
+}
+
+Mat4 m4_transpose(Mat4 m) {
+  return (Mat4){m.m[0][0], m.m[1][0], m.m[2][0], m.m[3][0],
+                m.m[0][1], m.m[1][1], m.m[2][1], m.m[3][1],
+                m.m[0][2], m.m[1][2], m.m[2][2], m.m[3][2],
+                m.m[0][3], m.m[1][3], m.m[2][3], m.m[3][3]};
+}
+
+Mat4 m4_invert(Mat4 m) { return (Mat4){0}; }
 
 void m4_print(Mat4 m4) {
   for (size_t row = 0; row < 4; row++)
