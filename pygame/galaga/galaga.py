538bc2c3c5e124f096ee7d95a7a937680081db8b
azimut <azimut.github@protonmail.com>
2026-09-25 21:03:35 -0300

    galaga: speed, woobly enemy

--- pygame/galaga/galaga.py
+++ pygame/galaga/galaga.py
@@ -1,6 +1,6 @@
 import random
 import pygame
-from pygame.locals import QUIT
+import math
 
 fps      = 60
 ventanaH = 700
@@ -13,8 +13,27 @@ class Enemigo:
     def __init__(self):
         self.image = pygame.image.load("imagenes/enemigo.png")
         self.ancho, self.alto = self.image.get_size()
+        self.x = random.randint(0,ventanaH-self.ancho)
+        self.y = -self.alto;
     def mostrar(self, ventana):
-        ventana.blit(self.image, (0,0))
+        ventana.blit(self.image, (self.x,self.y))
+    def mover(self):
+        self.y += 2
+        self.x += math.sin(self.y*0.02) * 3
+        self.x = min(max(self.x, 0), ventanaH - self.ancho)
+        if self.y > ventanaV:
+            self.y = -self.alto
+            self.x = random.randint(0,ventanaH-self.ancho)
+    def choca_con(self, otro): # otro.ancho otro.alto otro.x otro.y
+        xc_enemigo = self.x + self.ancho/2
+        yc_enemigo = self.y + self.ancho/2
+        r_enemigo = self.ancho/2
+        xc_otro = otro.x + otro.ancho/2
+        yc_otro = otro.y + otro.alto/2
+        r_otro = otro.ancho/2
+        distancia = math.sqrt( (xc_otro - xc_enemigo)**2 + (yc_otro - yc_enemigo)**2)
+        distancia_bordes = distancia - (r_enemigo + r_otro)
+        return distancia_bordes <= 0
 
 class Misil:
     def __init__(self):
@@ -26,7 +45,10 @@ class Misil:
         if self.y >= 0:
             self.y -= 4
     def mostrar(self, ventana):
-        if self.x > 0 and self.x < ventanaH and self.y > 0 and self.y < ventanaV:
+        if     self.x > 0 \
+           and self.x < ventanaH \
+           and self.y > 0 \
+           and self.y < ventanaV:
             ventana.blit(self.image, (self.x,self.y))
     def disparar(self, x, y):
         self.x = x
@@ -41,26 +63,23 @@ class Nave:
         self.y = ventanaV - self.alto - 20
         self.velx = 0
         self.vely = 0
-        self.accelx = 0
-        self.accely = 0
-        self.misiles = [Misil(),Misil(),Misil(),Misil(),Misil()]
+        self.misiles = [Misil(),Misil(),Misil(),Misil(),Misil(),Misil(),Misil()]
     def mostrar(self, ventana):
-        ventana.blit(self.imagen, (self.x - self.ancho, self.y))
         for misil in self.misiles:
             misil.mostrar(ventana)
+        ventana.blit(self.imagen, (self.x, self.y))
     def mover(self):
         self.x += self.velx
         self.y += self.vely
-        self.x = min(max(self.x, 0), ventanaH)
-        if self.y < 0:
-            self.y = 0
+        self.x = min(max(self.x, 0), ventanaH - self.ancho)
+        self.y = min(max(self.y, 0), ventanaV - self.alto)
         for misil in self.misiles:
             misil.mover()
     def disparar(self, ventana):
         for misil in self.misiles:
             if misil.y < 0:
-                misil.y = self.y - self.alto/2
-                misil.x = self.x - self.ancho/2 - misil.ancho/2
+                misil.y = self.y - misil.alto
+                misil.x = self.x + self.ancho/2 - misil.ancho/2
                 break
 
 
@@ -69,13 +88,14 @@ class Estrella:
         self.x = random.randint(0,ventanaH)
         self.y = random.randint(0,ventanaV)
         self.size = random.randint(0, 4)
+        self.accel = 0
     def mostrar(self, ventana):
-        pygame.draw.rect(ventana, blanco, (self.x,self.y,self.size, self.size))
+        pygame.draw.rect(ventana, blanco, (self.x,self.y,self.size, self.size + self.accel))
     def mover(self):
         # self.x = (self.x + 2*self.size*.2) % ventanaH
-        self.y = (self.y + 2*self.size*.2) % ventanaV
+        self.y = ((self.y + 2*self.size*.2)+self.accel) % ventanaV
         if self.x == 0 or self.y == 0:
-            self.size = random.randint(1,5)
+            self.size = random.randint(1,4)
 
 class Estrellas:
     def __init__(self, cantidad):
@@ -104,28 +124,38 @@ def main():
         nave.mostrar(ventana)
         nave.mover()
         enemigo.mostrar(ventana)
+        enemigo.mover()
+        for misil in nave.misiles:
+            if enemigo.choca_con(misil):
+                misil.y = -misil.alto
+                enemigo.y = ventanaV
+
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     nave.disparar(ventana)
                 if event.key == pygame.K_j:
-                    nave.velx -= 3
+                    nave.velx -= 5
                 if event.key == pygame.K_l:
-                    nave.velx += 3
+                    nave.velx += 5
+                if event.key == pygame.K_k:
+                    nave.vely += 5
                 if event.key == pygame.K_i:
                     nave.vely -= 3
-                if event.key == pygame.K_k:
-                    nave.vely += 3
+                    for estrella in estrellas.universo:
+                        estrella.accel = 5
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_i:
                     nave.vely = 0
+                    for estrella in estrellas.universo:
+                        estrella.accel = 0
                 if event.key == pygame.K_k:
                     nave.vely = 0
                 if event.key == pygame.K_j:
                     nave.velx = 0
                 if event.key == pygame.K_l:
                     nave.velx = 0
-            if event.type == QUIT:
+            if event.type == pygame.QUIT:
                 jugando = False
         pygame.display.flip()
         pygame.time.Clock().tick(fps)
